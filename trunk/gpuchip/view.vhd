7a7eda70d1b5ee53b6341b2dc168104cbde301e3
sejoon210 <sejoon210@507ec704-5f39-5b00-d30a-131b7ddeddae>
2010-11-05 05:46:08 +0000

    Updated view with the tested code from viewtest and changed the read and writes to the dualport adapter so that it only reads from the SDRAM and sends pixels to the view entity. (NOT FULLY FUNCTIONAL)

--- trunk/gpuchip/view.vhd
+++ trunk/gpuchip/view.vhd
@@ -1,3 +1,15 @@
+---------------------------------------------------------------------------
+--      BouncingBall.vhd                                                 --
+--      Viral Mehta                                                      --
+--      Spring 2005                                                      --
+--                                                                       --
+--      Modified by Stephen Kempf 03-01-2006                             --
+--                                03-12-2007                             --
+--      Fall 2008 Distribution                                         --
+--                                                                       --
+--      For use with ECE 385 Lab 9                                       --
+--      UIUC ECE Department                                              --
+---------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
@@ -21,7 +33,8 @@ package view_pckg is
            sync : out std_logic;
            blank : out std_logic;
            vs : out std_logic;
-           hs : out std_logic);
+           hs : out std_logic;
+           visible_out : out std_logic);
     end component view;
 end package view_pckg;
 
@@ -48,7 +61,8 @@ entity view is
            sync : out std_logic;
            blank : out std_logic;
            vs : out std_logic;
-           hs : out std_logic);
+           hs : out std_logic;
+           visible_out : out std_logic);
 end view;
 
 architecture Behavioral of view is
@@ -74,7 +88,8 @@ component Color_Mapper is
           
           Red   : out std_logic_vector(1 downto 0);
           Green : out std_logic_vector(1 downto 0);
-          Blue  : out std_logic_vector(1 downto 0));
+          Blue  : out std_logic_vector(1 downto 0);
+          visible: out std_logic);
 end component;
 
 signal rst, vsSig : std_logic;
@@ -86,6 +101,7 @@ signal fifo_rst, fifo_empty : std_logic;
 signal fifo_level : std_logic_vector(7 downto 0);
 signal   cke                        :     std_logic;
 signal cke_rd, rd_x, rd_r : std_logic;
+signal pixel_clk : std_logic;
 signal   pixel                      :     std_logic_vector(7 downto 0);
 signal   pixel_data_x, pixel_data_r :     std_logic_vector(15 downto 0);
 signal   pixel_data_out             :     std_logic_vector(15 downto 0);
@@ -95,30 +111,31 @@ signal   clk_div_cnt                :     unsigned(7 downto 0);
 
 begin
 
+visible_out <= visible;
 rst <= not Reset; -- The push buttons are active low
 --cke<='1';
-process(clk, rst)
-  begin
-    if rst = '1' then
-      clk_div_cnt   <= (others => '0');
-      cke           <= '1';
-    elsif rising_edge(clk) then
-      if clk_div_cnt = 3 then
-        clk_div_cnt <= (others => '0');
-        cke         <= '1';
-      else
-        clk_div_cnt <= clk_div_cnt + 1;
-        cke         <= '0';
-      end if;
-    end if;
-end process;
+--process(clk, rst)
+--  begin
+--    if rst = '1' then
+--      clk_div_cnt   <= (others => '0');
+--      cke           <= '1';
+--    elsif rising_edge(clk) then
+--      if clk_div_cnt = 1 then
+--        clk_div_cnt <= (others => '0');
+--        cke         <= '1';
+--      else
+--        clk_div_cnt <= clk_div_cnt + 1;
+--        cke         <= '0';
+--      end if;
+--    end if;
+--end process;
 
 vgaSync_instance : vga_controller
    Port map(clk => clk,
             reset => rst,
             hs => hs,
             vs => vsSig,
-            pixel_clk => VGA_clk,
+            pixel_clk => pixel_clk,
             blank => blank_i,
             sync => sync,
             DrawX => DrawXSig,
@@ -132,7 +149,8 @@ Color_instance : Color_Mapper
             B_in => b,
             Red => Red,
             Green => Green,
-            Blue => Blue);
+            Blue => Blue,
+            visible => visible);
 
   -- pixel data buffer
    cke_rd <= rd_x and cke;
@@ -145,19 +163,19 @@ fifo : fifo_cc
       data_in  => pixel_data_in,
       rst      => fifo_rst,
       data_out => pixel_data_out,
-      full     => open,
+      full     => full,
       empty    => fifo_empty,
       level    => fifo_level
       );
-  full   <= '1' when fifo_level(7 downto 3) = "11111" else '0';
+  --full   <= '1' when fifo_level(7 downto 3) = "11111" else '0';
             
 vs <= vsSig;
 blank <= blank_i;
+VGA_clk <= pixel_clk;
+cke <= pixel_clk;
 
   eof      <= eof_i;
   fifo_rst <= eof_i or rst;             -- clear the contents of the pixel buffer at the end of every frame
-
-  visible    <= blank_i;    -- pixels are visible when blank is inactive 
 
   -- get the current pixel from the word of pixel data or read more pixel data from the buffer
   get_pixel : process(visible, pixel_data_out, pixel_data_r, rd_r, DrawXSig, fifo_empty)
@@ -186,7 +204,7 @@ blank <= blank_i;
   end process get_pixel;
 
   -- map the current pixel to RGB values
-  map_pixel : process(pixel, rgb_r, blank_i)
+  map_pixel : process(pixel, rgb_r, visible)
   begin
     -- 8-bit pixels map directly to RGB values
     rgb_x <= pixel(7 downto 6) & pixel(4 downto 1);
@@ -212,7 +230,7 @@ blank <= blank_i;
       --blank_r        <= (others => '0');
       pixel_data_r   <= (others => '0');
       rgb_r          <= (others => '0');
-    elsif rising_edge(clk) then
+    elsif falling_edge(clk) then
       --eof_r          <= eof_x;          -- end-of-frame signal goes at full clock rate to external system
       if cke = '1' then
         rd_r         <= rd_x;
@@ -224,10 +242,10 @@ blank <= blank_i;
     end if;
   end process update;
   
-  eof_proc : process(DrawYSig)
+  eof_proc : process(DrawXSig,DrawYSig)
   begin
 	if(rising_edge(clk)) then
-		if(DrawYSig = 239) then
+		if(DrawYSig = conv_std_logic_vector(240,10) and DrawXSig = conv_std_logic_vector(320,10)) then
 			eof_i <= '1';
 		else
 			eof_i <= '0';
