66d338e3b4b2d679a7b9634bd72160c27d5c410d
sejoon210 <sejoon210@507ec704-5f39-5b00-d30a-131b7ddeddae>
2010-11-24 06:35:34 +0000

    Updated altera_memtest (read is broken FIXME)

--- trunk/altera_memtest/memtest.vhd
+++ trunk/altera_memtest/memtest.vhd
@@ -30,11 +30,11 @@ architecture behavior of memtest is
     signal state_cur, state_nxt: testState;
 
     signal cnt :std_logic_vector(23 downto 0);
-    signal addr_cnt: std_logic_vector(21 downto 0);
+    signal addr_cnt_x, addr_cnt_y: std_logic_vector(21 downto 0);
     signal en_cnt:std_logic;
 	signal skip_wrt : std_logic;
 	signal state_num_internal : std_logic_vector(3 downto 0);
-	signal to_data_out : std_logic_vector(15 downto 0);
+	signal data_chk_x, data_chk_y : std_logic_vector(15 downto 0);
 
 begin
     --Wires connection
@@ -42,8 +42,8 @@ begin
 	skip_wrt <= skip_write;
     rst <= reset;
     errcnt <= cnt;
-    addr <= addr_cnt;
-    data_out <= addr_cnt(15 downto 0);
+    addr <= addr_cnt_y;
+    data_out <= addr_cnt_y(15 downto 0);
     be_n <= "00";
     cs <= '1';
 	state_num <= state_num_internal;
@@ -56,38 +56,45 @@ begin
 				state_cur<=INIT_R;
         elsif (rising_edge(clk)) then
             state_cur<=state_nxt;
+            addr_cnt_y <= addr_cnt_x;
+            data_chk_y <= data_chk_x;
         end if;
     end process;
 
     address_cnt:process (state_cur, clk)
     begin
         if(state_cur=INIT_W or state_cur=INIT_R) then
-            addr_cnt<= conv_std_logic_vector(0,22);
+            addr_cnt_x<= conv_std_logic_vector(0,22);
         elsif (rising_edge(clk)) then
             if(state_cur=WRITE_INC or state_cur=READ_INC) then
-                addr_cnt<=addr_cnt+1;
+                addr_cnt_x<=addr_cnt_x+1;
             else
-                addr_cnt<=addr_cnt;
+                addr_cnt_x<=addr_cnt_x;
             end if;
         end if;
     end process;
 
-    error_cnt: process(state_cur, clk, addr_cnt, data_in)
+    error_cnt: process(state_cur, clk, data_chk_y, data_in)
     begin
-        if(state_cur=INIT_W) then
+        if(state_cur=INIT_R) then
             cnt<= conv_std_logic_vector(0,24);
+            data_chk_x<= conv_std_logic_vector(0,16);
         elsif (rising_edge(clk)) then
-            if (state_cur= READ_INC) then
             --currently valid data is at the previous address in SDRAM
-				if(rvalid='1') then
-					if ((addr_cnt(7 downto 0)-1) /= data_in(7 downto 0)) then
-						cnt<=cnt+1;
-					end if;
-            end if;
-        end if;
+			if(rvalid='1') then
+				data_chk_x <= data_chk_x + 1;
+				if (data_chk_y /= data_in(7 downto 0)) then
+					cnt <= cnt+1;
+				else
+					cnt <= cnt;
+				end if;
+			else
+				data_chk_x <= data_chk_x;
+			end if;
+		end if;
     end process;
 	 
-    next_state: process (state_cur, start, waitreq, rvalid, addr_cnt)
+    next_state: process (state_cur, start, waitreq, rvalid, addr_cnt_y)
     begin
         case state_cur is
             when INIT_W => 
@@ -103,7 +110,7 @@ begin
                 rd <= '0';
                 wr <= '1';
 				--data_out <= x"ABCD";
-					 --we need to keep outputting current address if waitreq is high
+				--we need to keep outputting current address if waitreq is high
                 if (waitreq = '1') then
                     state_nxt<= WRITE_S;
                 else
@@ -113,48 +120,47 @@ begin
             when WRITE_INC=>
                 rd <= '0';
                 wr <= '1';
-					 state_nxt<=WRITE_CHK;
-					 state_num_internal <= x"2";
-			   when WRITE_CHK=>
-				 rd <= '0';
-				 wr <= '1';
-				 if (addr_cnt = x"3FFFFF") then
+				state_nxt<=WRITE_CHK;
+				state_num_internal <= x"2";
+			when WRITE_CHK=>
+				rd <= '0';
+				wr <= '1';
+				if (addr_cnt_y = x"3FFFFF") then
 					state_nxt<=INIT_R;
-				 else
+				else
 					state_nxt<=WRITE_S;
-				 end if;
-				 state_num_internal <= x"3";
+				end if;
+				state_num_internal <= x"3";
             when INIT_R =>
                 rd <= '0';
                 wr <= '0';
-					 if (start ='1') then 
+				if (start ='1') then 
                     state_nxt<= READ_S;
                 else 
                     state_nxt<= INIT_R;
                 end if;
-					 state_num_internal <= x"4";
+				state_num_internal <= x"4";
             when READ_S =>
                 rd <= '1';
                 wr <= '0';
-                --if (rvalid='1' and waitreq='0') then
-					 --we need to keep outputting current address if waitreq is high
+				--we need to keep outputting current address if waitreq is high
 				if (waitreq='1') then
                     state_nxt<= READ_S;
                 else
                     state_nxt<= READ_INC;
                 end if;
-					 state_num_internal <= x"5";
+				state_num_internal <= x"5";
             when READ_INC=>
                 rd <= '1';
                 wr <= '0';
-                if (addr_cnt = x"3FFFFF") then
+                if (addr_cnt_y = x"3FFFFF") then
                     state_nxt<= REST;
-	                else
+	            else
                     state_nxt<=READ_S;
                 end if;
-					 state_num_internal <= x"6";
+				state_num_internal <= x"6";
             when REST=>
-                rd <= '1';
+                rd <= '0';
                 wr <= '0';
                 state_nxt<= REST;
 				state_num_internal <= x"7";
