ca5260b6a503e4d140ef08f7fba8728a63b0e60f
sejoon210 <sejoon210@507ec704-5f39-5b00-d30a-131b7ddeddae>
2010-12-04 10:38:40 +0000

    Added the SDRAM component to the viewtest, but it DOES NOT work. Needs to be simulated and fixed.

--- trunk/viewtest_sram/viewtest.vhd
+++ trunk/viewtest_sram/viewtest.vhd
@@ -4,6 +4,8 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use WORK.view_pckg.ALL;
 use WORK.HexDriver_pckg.ALL;
+use WORK.sdram.ALL;
+use WORK.sdram_pll_pckg.ALL;
 
 ENTITY viewtest IS 
 	PORT
@@ -22,31 +24,58 @@ ENTITY viewtest IS
 		VGA_G :  OUT  STD_LOGIC_VECTOR(9 DOWNTO 8);
 		VGA_R :  OUT  STD_LOGIC_VECTOR(9 DOWNTO 8);
 		HEX0 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+		--SRAM Connections
 		SRAM_ADDR : OUT STD_LOGIC_VECTOR(17 downto 0);
 		SRAM_DQ : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
 		SRAM_WE_N : OUT STD_LOGIC;
 		SRAM_OE_N : OUT STD_LOGIC;
 		SRAM_UB_N : OUT STD_LOGIC;
 		SRAM_LB_N : OUT STD_LOGIC;
-		SRAM_CE_N : OUT STD_LOGIC
+		SRAM_CE_N : OUT STD_LOGIC;
+		--SDRAM Connections
+		DRAM_CLK : out std_logic;
+		DRAM_CKE : out std_logic;
+		DRAM_CS_N : out std_logic;
+		DRAM_RAS_N : out std_logic;
+		DRAM_CAS_N : out std_logic;
+		DRAM_WE_N : out std_logic;
+		DRAM_BA_1 : out std_logic;
+		DRAM_BA_0 : out std_logic;
+		DRAM_ADDR : out std_logic_vector(11 downto 0);
+		DRAM_DQ : inout std_logic_vector(15 downto 0);
+		DRAM_UDQM : out std_logic;
+		DRAM_LDQM : out std_logic
 	);
 END viewtest;
 
 architecture behavioral of viewtest is
 
-signal wr : std_logic;
+signal clk : std_logic;
+signal rd : std_logic; --read from SDRAM
+signal wr : std_logic; --write to SRAM
 signal pixel : std_logic_vector(15 downto 0);
 signal count : std_logic_vector(15 downto 0);
 signal visible : std_logic;
 signal vga_clk_in : std_logic;
-signal rst_i : std_logic;
+signal rst_i, nrst_i : std_logic;
 signal eof_i : std_logic;
 signal full_i : std_logic;
 
+--SDRAM related signals
+type sdramState is (HOLD, READ_S);
+signal state_cur, state_next : sdramState;
+signal addr_cur, addr_next : std_logic_vector(21 downto 0);
+signal earlyOpBegun_i : std_logic;
+signal opBegun_i : std_logic;
+signal rdPending_i : std_logic;
+signal done_i : std_logic;
+signal rdDone_i : std_logic;
+signal locked : std_logic;
+
 begin
 	u1: view
-	PORT MAP(Clk => CLOCK_50,
-			 nReset => rst_i,
+	PORT MAP(Clk => clk,
+			 nReset => nrst_i,
 			 wr => wr,
 			 field_color => SW(7 downto 0),
 			 pixel_data_in => pixel,
@@ -68,26 +97,92 @@ begin
 			 pin_sram_ub_n => SRAM_UB_N,
 			 pin_sram_lb_n => SRAM_LB_N,
 			 pin_sram_ce_n => SRAM_CE_N);
+			 
+	u2 : sdramCntl
+	port map(
+		-- host side input
+		clk =>		clk,
+		lock =>		locked,
+		rst =>		rst_i,
+		rd =>		rd,
+		wr =>		'0',
+		hAddr =>	addr_cur,
+		hDIn => 	x"0000",
+		-- host side output
+		hDOut =>		pixel,
+		earlyOpBegun => earlyOpBegun_i,
+		opBegun => 		opBegun_i,
+		rdPending =>	rdPending_i,
+		done=>			done_i,
+		rdDone=>		rdDone_i,
+		status =>		open,
+		
+		-- SDRAM side
+		cke =>		DRAM_CKE,
+		ce_n => 	DRAM_CS_N,
+		ras_n =>	DRAM_RAS_N,
+		cas_n =>	DRAM_CAS_N,
+		we_n =>		DRAM_WE_N,
+		ba(1) =>	DRAM_BA_1,
+		ba(0) =>	DRAM_BA_0,
+		sAddr =>	DRAM_ADDR,
+		sData =>	DRAM_DQ,
+		dqmh =>		DRAM_UDQM,
+		dqml =>		DRAM_LDQM);
+		
+	u3: sdram_pll
+	port map(
+			inclk0	=> CLOCK_50,
+			c0		=> DRAM_CLK,
+			c1		=> clk,
+			locked	=> locked 
+		);
 	
---	u2: HexDriver
+--	u4: HexDriver
 --	Port map( In0 => "000" & full_i,
 --			  Out0 => HEX0);
 			 
 VGA_CLK <= vga_clk_in;
-rst_i <= KEY(0);
+rst_i <= not KEY(0);
+nrst_i <= KEY(0);
 LEDR(0) <= eof_i;
---wr <= not full_i and not visible;
-wr <= '0';
-			 
-count_pixel : process (CLOCK_50, eof_i, full_i, rst_i)
+wr <= not full_i and rdDone_i;
+
+update : process (clk,rst_i)
 begin
-	if(eof_i = '1' or rst_i = '0') then
-		count <= x"0000";
-	elsif rising_edge(CLOCK_50) and full_i='0' then
-		count <= count+1;
+	if(rst_i ='1' or visible ='1') then
+		state_cur <= HOLD;
+		addr_cur <= (others=>'0');
+	elsif (rising_edge(clk)) then
+		state_cur <= state_next;
+		addr_cur <= addr_next;
 	end if;
 end process;
 
-    pixel <= x"FFFF" when count = SW else x"0000";
-    pixelval <= pixel;
+next_state: process (eof_i,state_cur, done_i, rdDone_i, addr_next)
+begin
+	case state_cur is
+		when HOLD => 
+			rd <= '0';
+			addr_next <= (others=>'0');
+			if (eof_i ='1') then 
+				state_next<= READ_S;
+			else 
+				state_next<= HOLD;
+			end if;
+		when READ_S =>
+			rd <= '1';
+			if (rdDone_i = '1') then
+				addr_next <= addr_next+1;
+				if (addr_next = "1001011000000000") then -- if address is equal to x"9600"
+					state_next <= HOLD;
+				else
+					state_next <= READ_S;
+				end if;
+			else
+				addr_next <= addr_next;
+				state_next <= READ_S;
+			end if;
+	end case;
+end process;
 end;
