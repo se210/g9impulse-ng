ea8ff57c4239f4494e0d55fe6c53c68dbdfd24a0
sejoon210 <se210@users.noreply.github.com>
2010-12-13 02:29:51 +0000

    Final version of the gpuchip

---
+++ gpuchip_final/blitter.vhd
@@ -0,0 +1,355 @@
+--ECE395 GPU:
+--Blitter Subunit
+--=====================================================
+--Designed by:
+--Se-Joon Chung
+--Wenxun Huang
+--Chun Yang
+--
+--of the University of Illinois at Urbana Champaign
+--under the direction of Dr. Lippold Haken
+--====================================================
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+package blitter_pckg is
+    component blitter is
+        port (
+                clk     : in std_logic;
+                reset   : in std_logic;
+                -- signals to SDRAM
+                sdram_addr      : out std_logic_vector (21 downto 0);
+                sdram_be_n      : out std_logic_vector (1 downto 0);
+                sdram_cs        : out std_logic;
+                sdram_wr_data   : out std_logic_vector (15 downto 0);
+                sdram_rd_n      : out std_logic;
+                sdram_wr_n      : out std_logic;
+                -- signals from SDRAM
+                sdram_rd_data       : in std_logic_vector (15 downto 0);
+                sdram_valid         : in std_logic;
+                sdram_waitrequest   : in std_logic;
+                -- signals to SRAM
+                sram_addr   : out std_logic_vector(17 downto 0);
+                sram_dq     : out std_logic_vector(15 downto 0);
+                sram_we_n   : out std_logic;
+                sram_oe_n   : out std_logic;
+                sram_ub_n   : out std_logic;
+                sram_lb_n   : out std_logic;
+                sram_ce_n   : out std_logic;
+                sram_be_n   : out std_logic_vector(1 downto 0);
+                -- signals from SRAM
+                sram_waitrequest    : in std_logic;
+                -- signals from gpuchip
+                blit_begin      : in std_logic;
+                source_address  : in std_logic_vector(23 downto 0); -- SDRAM
+                target_address  : in std_logic_vector(23 downto 0); -- SRAM
+                source_lines    : in std_logic_vector(7 downto 0);
+                line_size       : in std_logic_vector(7 downto 0);
+                alpha_op        : in std_logic;
+                front_buffer    : in std_logic;
+                -- signals to gpuchip
+                blit_done   : out std_logic;
+                read_state  : out std_logic_vector(3 downto 0);
+                write_state : out std_logic_vector(3 downto 0)
+            );
+    end component blitter;
+end blitter_pckg;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use work.common.all;
+use work.blitter_fifo_pckg.all;
+use work.fifo_cc_pckg.all;
+
+entity blitter is
+    port (
+            clk     : in std_logic;
+            reset   : in std_logic;
+            -- signals to SDRAM
+            sdram_addr      : out std_logic_vector (21 downto 0);
+            sdram_be_n      : out std_logic_vector (1 downto 0);
+            sdram_cs        : out std_logic;
+            sdram_wr_data   : out std_logic_vector (15 downto 0);
+            sdram_rd_n      : out std_logic;
+            sdram_wr_n      : out std_logic;
+            -- signals from SDRAM
+            sdram_rd_data       : in std_logic_vector (15 downto 0);
+            sdram_valid         : in std_logic;
+            sdram_waitrequest   : in std_logic;
+            -- signals to SRAM
+            sram_addr   : out std_logic_vector(17 downto 0);
+            sram_dq     : inout std_logic_vector(15 downto 0);
+            sram_we_n   : out std_logic;
+            sram_oe_n   : out std_logic;
+            sram_ub_n   : out std_logic;
+            sram_lb_n   : out std_logic;
+            sram_ce_n   : out std_logic;
+            sram_be_n   : out std_logic_vector(1 downto 0);
+            -- signals from SRAM
+            sram_waitrequest    : in std_logic;
+            -- signals from gpuchip
+            blit_begin      : in std_logic;
+            source_address  : in std_logic_vector(23 downto 0); -- SDRAM
+            target_address  : in std_logic_vector(23 downto 0); -- SRAM
+            source_lines    : in std_logic_vector(7 downto 0);
+            line_size       : in std_logic_vector(7 downto 0);
+            alpha_op        : in std_logic;
+            front_buffer    : in std_logic;
+            -- signals to gpuchip
+            blit_done   : out std_logic;
+            read_state  : out std_logic_vector(3 downto 0);
+            write_state : out std_logic_vector(3 downto 0)
+
+        );
+end blitter;
+
+architecture behavior of blitter is
+    type readState is (
+        STANDBY,
+        INIT,
+        INIT_LINE,
+        READ,
+        CONTINUE
+    );
+    signal read_state_r, read_state_x   : readState;
+
+    type writeState is (
+        STANDBY,
+        INIT,
+        WRITE,
+        CONTINUE
+    );
+    signal write_state_r, write_state_x : writeState;
+
+    -- registers
+    -- FIXME
+    signal read_line_r, read_line_x : std_logic_vector(7 downto 0);
+    signal read_count_r, read_count_x : std_logic_vector(7 downto 0);
+    signal read_addr_r, read_addr_x : std_logic_vector(23 downto 0);
+
+    signal s_addr_r, s_addr_x   : std_logic_vector(23 downto 0);
+    signal t_addr_r, t_addr_x   : std_logic_vector(23 downto 0);
+
+    signal write_line_r, write_line_x   : std_logic_vector(7 downto 0);
+    signal write_count_r, write_count_x : std_logic_vector(7 downto 0);
+    signal write_addr_r, write_addr_x   : std_logic_vector(23 downto 0);
+
+    -- fifo signals
+    signal rd_q     : std_logic;
+    signal wr_q     : std_logic;
+    signal full_q   : std_logic;
+    signal empty_q  : std_logic;
+    signal level_q  : std_logic_vector(7 downto 0);
+    signal wr_q_en  : std_logic;
+    signal out_q    : std_logic_vector(15 downto 0);
+    signal out_q_buf    : std_logic_vector(15 downto 0);
+
+    signal sdram_rd : std_logic;
+    signal sram_we  : std_logic;
+    signal sram_be  : std_logic_vector(1 downto 0);
+
+    function blend(data : std_logic_vector(15 downto 0); alpha_op : std_logic) return std_logic_vector is
+        variable byte_enable : std_logic_vector(1 downto 0) := "11";
+    begin
+        if (alpha_op = '1') then
+            if (data(15 downto 8) = x"E7") then
+                byte_enable(1) := NO;
+            end if;
+
+            if (data(7 downto 0) = x"E7") then
+                byte_enable(0) := NO;
+            end if;
+        end if;
+        return byte_enable;
+    end function blend;
+    
+begin
+	read_state <= write_addr_r(3 downto 0);
+	write_state <= write_addr_r(7 downto 4);
+-- DO NOT USE (blitter_fifo is broken)
+--    fifo_inst : blitter_fifo
+--    port map (  clock => clk,
+--                data => sdram_rd_data,
+--                rdreq => rd_q,
+--                wrreq => wr_q,
+--                almost_full => full_q,
+--                empty => empty_q,
+--                full => open,
+--                q => out_q,
+--                usedw => open
+--             );
+	fifo_inst : fifo_cc
+	port map ( 
+	  clk		=> clk,
+      rst		=> '0',
+      rd        => rd_q,
+      wr        => wr_q,
+      data_in   => sdram_rd_data,
+      data_out  => out_q,
+      full      => open,
+      empty     => empty_q,
+      level     => level_q
+			);
+	full_q <= '1' when level_q = "11110000" else '0';
+
+    -- SDRAM outputs
+    sdram_addr  <= read_addr_r(21)&read_addr_r(19 downto 8)&read_addr_r(20)&read_addr_r(7 downto 0);
+    sdram_wr_n  <= '1';     -- disable writes
+    sdram_be_n  <= "00";    -- enable both bytes
+    sdram_cs    <= '1';     -- chip select??
+    sdram_wr_data <= (others => 'Z');
+    sdram_rd_n  <= not sdram_rd;
+
+    -- SRAM outputs
+    sram_addr <= write_addr_r(17 downto 0);
+    sram_oe_n <= '1';   -- no reading by blitter
+    sram_ce_n <= '0';   -- chip enable??
+    sram_we_n <= not sram_we;
+    sram_be <= blend(out_q, alpha_op);
+    sram_ub_n <= not sram_be(1);
+    sram_lb_n <= not sram_be(0);
+    sram_dq <= out_q;
+    sram_be_n <= not sram_be;
+
+    -- GPU outputs
+    blit_done <= YES when read_state_r = CONTINUE and
+                         write_state_r = CONTINUE else NO;
+
+    -- fifo write?
+    --wr_q <= wr_q_en and sdram_valid;
+    wr_q <= sdram_valid;
+
+    read_comb : process (read_state_r, s_addr_r, read_line_r, read_addr_r,
+                         read_count_r, sdram_waitrequest, sdram_valid,
+                         source_address, full_q, source_lines,
+                         line_size, blit_begin)
+    begin
+        sdram_rd    <= NO;
+        wr_q_en     <= NO;
+
+        s_addr_x    <= s_addr_r;
+
+        read_line_x     <= read_line_r;
+        read_count_x    <= read_count_r;
+        read_addr_x     <= read_addr_r;
+        read_state_x    <= read_state_r;
+
+        case read_state_r is
+            when STANDBY =>
+                if (blit_begin = YES) then
+                    read_state_x <= INIT;
+                end if;
+
+            when INIT =>
+                s_addr_x <= source_address;
+                read_addr_x <= source_address;
+                read_line_x <= x"00";
+                read_count_x <= x"00";
+                read_state_x <= READ;
+
+            when READ =>
+                wr_q_en <= YES;
+                if (sdram_waitrequest = NO and full_q = NO) then
+                    sdram_rd <= YES;
+
+                    if (read_count_r = line_size - '1') then
+                        read_count_x    <= x"00";
+                        read_line_x     <= read_line_r + '1';
+                        read_addr_x     <= s_addr_r + x"0000A0";
+                        s_addr_x        <= s_addr_r + x"0000A0";
+                    else
+                        read_addr_x <= read_addr_r + '1';
+                        read_count_x <= read_count_r + '1';
+                    end if;
+                end if;
+
+                if (read_line_r = source_lines) then
+                    sdram_rd <= NO;
+                    read_state_x <= CONTINUE;
+                end if;
+            when others =>
+				NULL;
+        end case;
+    end process read_comb;
+
+    write_comb : process (write_state_r, t_addr_r, write_line_r, write_addr_r,
+                          write_count_r, target_address, empty_q,
+                          source_lines, line_size, blit_begin,
+                          front_buffer, sram_waitrequest)
+    begin
+        rd_q <= NO;
+
+        t_addr_x <= t_addr_r;
+
+        write_line_x    <= write_line_r;
+        write_count_x   <= write_count_r;
+        write_addr_x    <= write_addr_r;
+        write_state_x   <= write_state_r;
+
+        case write_state_r is
+            when STANDBY =>
+                if (blit_begin = YES) then
+                    write_state_x <= INIT;
+                end if;
+
+            when INIT =>
+                t_addr_x <= target_address;
+                write_addr_x <= target_address;
+                write_line_x <= x"00";
+                write_count_x <= x"00";
+                write_state_x <= WRITE;
+
+            when WRITE =>
+                if (empty_q = NO and sram_waitrequest = NO) then
+                    rd_q <= YES;
+
+                    if (write_count_r = line_size - '1') then
+                        write_count_x   <= x"00";
+                        write_line_x    <= write_line_r + '1';
+                        write_addr_x    <= t_addr_r + x"0000A0";
+                        t_addr_x        <= t_addr_r + x"0000A0";
+                    else
+                        write_addr_x    <= write_addr_r + '1';
+                        write_count_x   <= write_count_r + '1';
+                    end if;
+                end if;
+                if (write_line_r = source_lines) then
+                    write_state_x <= CONTINUE;
+                else
+                	write_state_x <= WRITE;
+                end if;
+
+            when others =>
+	    	NULL;
+        end case;
+    end process write_comb;
+
+    update : process (clk, reset, sram_waitrequest)
+    begin
+        if (reset = '1') then
+			read_state_r <= STANDBY;
+			write_state_r <= STANDBY;
+		elsif (rising_edge(clk)) then
+			s_addr_r <= s_addr_x;
+			t_addr_r <= t_addr_x;
+
+			read_line_r <= read_line_x;
+			read_count_r <= read_count_x;
+			read_addr_r <= read_addr_x;
+
+			read_state_r <= read_state_x;
+			write_state_r <= write_state_x;
+
+            write_line_r <= write_line_x;
+            write_count_r <= write_count_x;
+            write_addr_r <= write_addr_x;
+            if (sram_waitrequest = NO) then
+                sram_we <= rd_q;
+            end if;
+
+        end if;
+    end process update;
+end behavior;
---
+++ gpuchip_final/view.vhd
@@ -0,0 +1,260 @@
+--ECE395 GPU:
+--View Subunit
+--=====================================================
+--Designed by:
+--Se-Joon Chung
+--Wenxun Huang
+--Chun Yang
+--
+--of the University of Illinois at Urbana Champaign
+--under the direction of Dr. Lippold Haken
+--====================================================
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+package view_pckg is
+	component view is
+    Port ( Clk : in std_logic;
+           nReset : in std_logic;
+           field_color 	  : in std_logic_vector(7 downto 0);
+
+		   sof			   : out std_logic;
+		   eof             : out std_logic;    -- end of vga frame         
+           Red   : out std_logic_vector(1 downto 0);
+           Green : out std_logic_vector(1 downto 0);
+           Blue  : out std_logic_vector(1 downto 0);
+           VGA_clk : out std_logic; 
+           sync : out std_logic;
+           blank : out std_logic;
+           vs : out std_logic;
+           hs : out std_logic;
+           visible_out : out std_logic;
+           
+           --SRAM interface
+           wr : in  std_logic;    -- write-enable for pixel buffer
+           wr_addr : in std_logic_vector(17 downto 0); -- adress to write to in SRAM
+           wr_be_n : in std_logic_vector(1 downto 0); -- byte-enable mask for writing
+           wait_request : out std_logic; -- wait request for the write
+           pixel_data_in   : in  std_logic_vector(15 downto 0);  -- input databus to pixel buffer
+           pin_sram_addr : out std_logic_vector(17 downto 0);
+           pin_sram_dq : inout std_logic_vector(15 downto 0);
+           pin_sram_we_n : out std_logic;
+           pin_sram_oe_n : out std_logic;
+           pin_sram_ub_n : out std_logic;
+           pin_sram_lb_n : out std_logic;
+           pin_sram_ce_n : out std_logic
+           );
+    end component view;
+end package view_pckg;
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity view is
+    Port ( Clk : in std_logic;
+           nReset : in std_logic;
+           field_color 	  : in std_logic_vector(7 downto 0);
+
+		   sof			   : out std_logic;
+		   eof             : out std_logic;    -- end of vga frame        
+           Red   : out std_logic_vector(1 downto 0);
+           Green : out std_logic_vector(1 downto 0);
+           Blue  : out std_logic_vector(1 downto 0);
+           VGA_clk : out std_logic; 
+           sync : out std_logic;
+           blank : out std_logic;
+           vs : out std_logic;
+           hs : out std_logic;
+           visible_out : out std_logic;
+           
+           --SRAM interface
+           wr : in  std_logic;    -- write-enable for pixel buffer
+           wr_addr : in std_logic_vector(17 downto 0); -- adress to write to in SRAM
+           wr_be_n : in std_logic_vector(1 downto 0); -- byte-enable mask for writing
+           wait_request : out std_logic; -- wait request for the write
+           pixel_data_in   : in  std_logic_vector(15 downto 0);  -- input databus to pixel buffer
+           pin_sram_addr : out std_logic_vector(17 downto 0);
+           pin_sram_dq : inout std_logic_vector(15 downto 0);
+           pin_sram_we_n : out std_logic;
+           pin_sram_oe_n : out std_logic;
+           pin_sram_ub_n : out std_logic;
+           pin_sram_lb_n : out std_logic;
+           pin_sram_ce_n : out std_logic
+           );
+end view;
+
+architecture Behavioral of view is
+
+component vga_controller is
+    Port ( clk : in std_logic;
+           reset : in std_logic;
+           hs : out std_logic;
+           vs : out std_logic;
+           pixel_clk : out std_logic;
+           blank : out std_logic;
+           sync : out std_logic;
+           DrawX : out std_logic_vector(9 downto 0);
+           DrawY : out std_logic_vector(9 downto 0));
+end component;
+
+component Color_Mapper is
+   Port ( DrawX : in std_logic_vector(9 downto 0);
+          DrawY : in std_logic_vector(9 downto 0);
+          R_in : in std_logic_vector(1 downto 0);
+          G_in : in std_logic_vector(1 downto 0);
+          B_in : in std_logic_vector(1 downto 0);
+          
+          Red   : out std_logic_vector(1 downto 0);
+          Green : out std_logic_vector(1 downto 0);
+          Blue  : out std_logic_vector(1 downto 0);
+          visible: out std_logic);
+end component;
+
+signal rst, vsSig : std_logic;
+signal DrawXSig, DrawYSig : std_logic_vector(9 downto 0);
+signal r,g,b : std_logic_vector(1 downto 0);
+signal blank_i : std_logic;
+signal sof_i: std_logic;
+signal eof_i : std_logic;
+signal pixel_clk : std_logic;
+signal pixel_data_out             :     std_logic_vector(15 downto 0);
+signal visible : std_logic;
+signal duo_pixel_r : std_logic_vector(15 downto 0);
+signal current_pixel : std_logic_vector(7 downto 0);
+
+--SRAM related signals
+signal rd : std_logic;
+signal rd_addr  : std_logic_vector(17 downto 0) := (others=>'0');
+signal wr_allow : std_logic;
+
+begin
+
+visible_out <= visible;
+rst <= not nReset; -- The push buttons are active low
+
+vgaSync_instance : vga_controller
+   Port map(clk => clk,
+            reset => rst,
+            hs => hs,
+            vs => vsSig,
+            pixel_clk => pixel_clk,
+            blank => blank_i,
+            sync => sync,
+            DrawX => DrawXSig,
+            DrawY => DrawYSig);
+
+Color_instance : Color_Mapper
+   Port Map(DrawX => DrawXSig,
+            DrawY => DrawYSig,
+            R_in => r,
+            G_in => g,
+            B_in => b,
+            Red => Red,
+            Green => Green,
+            Blue => Blue,
+            visible => visible);
+  
+  
+  combinatorial : process (clk, visible, pixel_clk, DrawXSig, DrawYSig)
+  begin	
+	if(rising_edge(pixel_clk)) then
+		if((DrawXSig >= 0) and (DrawXSig < 320) and (DrawYSig >= 0) and (DrawYSig < 240)) then
+			rd <= '1';
+			wr_allow <= '0';
+		elsif(DrawXSig = 799 and DrawYSig = 524) then
+			rd <= '1';
+			wr_allow <= '0';
+		else
+			if(DrawYSig > conv_std_logic_vector(239,10) and DrawYSig < conv_std_logic_vector(800,10)) then
+				rd <= '0';
+				wr_allow <= '1';
+			elsif(DrawYSig = conv_std_logic_vector(239,10) and DrawXSig > conv_std_logic_vector(320,10)) then
+				rd <= '0';
+				wr_allow <= '1';
+			else
+				rd <= '0';
+				wr_allow <= '0';
+			end if;
+		end if;
+	end if;
+  end process;
+  
+  process(pixel_clk)
+  begin
+	if(rising_edge(pixel_clk)) then
+		if(DrawXSig = conv_std_logic_vector(799,10) and DrawYSig = conv_std_logic_vector(524,10)) then
+			rd_addr <= (others=>'0');
+		elsif(visible='1') then
+			if(DrawXSig(0) = '1') then
+				rd_addr <= rd_addr+1;
+			end if;
+		end if;
+	end if;
+  end process;
+  
+  wait_request <= not wr_allow;
+				
+  
+	-- memory read/write processes
+	-- reading from memory has priority over writing to memory
+	
+	pixel_data_out <= pin_sram_dq;
+	
+	Mem_Write : process (wr, wr_allow, pixel_data_in) is
+	begin
+	   pin_sram_dq <= "ZZZZZZZZZZZZZZZZ";
+	   if (wr_allow = '1' and wr = '1') then
+		  pin_sram_dq <= pixel_data_in;
+	   else
+		  null;
+	   end if;
+	end process;
+	
+  --connections to SRAM
+   pin_sram_addr <= wr_addr when (wr_allow='1' and wr='1') else rd_addr;
+   pin_sram_we_n <= not (wr and wr_allow);
+   pin_sram_oe_n <= not rd;
+   pin_sram_ub_n <= wr_be_n(1) when (wr_allow='1' and wr='1') else '0';
+   pin_sram_lb_n <= wr_be_n(0) when (wr_allow='1' and wr='1') else '0';
+   pin_sram_ce_n <= '0';  
+            
+            
+  --other VGA stuff
+  vs <= vsSig;
+  blank <= blank_i;
+  VGA_clk <= pixel_clk;
+
+  sof	   <= sof_i;
+  eof      <= eof_i;
+
+  --Wenxun wrote this, blame him
+  grab_pixel : process(clk, pixel_clk, DrawXSig, rst, eof_i)
+  begin
+      if(rst='1' or eof_i='1') then
+          duo_pixel_r<=x"0000";
+      elsif falling_edge(clk) and pixel_clk='0' and DrawXSig(0)='0' then
+      -- Together with the set_read process a word is read right before it's needed for the even pixel
+      -- I used falling edge because that's when the other two signals are stable
+		  duo_pixel_r <= pixel_data_out;
+      end if;
+  end process;
+
+  --Not sure about Endian, probably need to be fixed later. (Now higher byte to the left of lower byte on screen)
+  current_pixel<= duo_pixel_r(15 downto 8) when DrawXSig(0)='0' else duo_pixel_r(7 downto 0);
+  r <= current_pixel(7 downto 6);
+  g <= current_pixel(4 downto 3);
+  b <= current_pixel(2 downto 1);
+
+  -- (524, 799) is the last coordinate output from the vgasync component, which indicates the advent of the first pixel
+  -- on the screen.
+  sof_i <= '1' when DrawXSig = conv_std_logic_vector(799, 10) and DrawYSig = conv_std_logic_vector(524, 10) else'0';
+
+  -- (319, 239) is the last coordinate in the frame we actually draw, so eof goes high one pixel after that
+  eof_i <= '1' when DrawXSig = conv_std_logic_vector(320,10) and DrawYSig = conv_std_logic_vector(239,10) else '0';
+
+end Behavioral;      
